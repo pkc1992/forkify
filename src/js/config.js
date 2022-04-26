8bac421fa2b2048ccbe12661cae978e1f7dc5f9f
Kim <kchi.cnn@gmail.com>
2022-04-27 03:58:24 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2c359384-d925-4398-9420-2eb7085d2cbd';
+export const MODAL_CLOSE_SEC = 2.5;
